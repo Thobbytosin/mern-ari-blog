d977decb8994a3a5df0e335eadd765460a0722f2
Falode Tobi <thobbytosin11@gmail.com>
2024-04-11 06:53:22 +0100

    connect database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to Database successfully!!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 300");
+  console.log("Server is running on port 3000!!");
 });
+
+// gabrieltobiloba11
+// cH2wuMQg5EZwfb8I
+// mongodb+srv://gabrieltobiloba11:cH2wuMQg5EZwfb8I@mern-ari-blog.24ix8qn.mongodb.net/?retryWrites=true&w=majority&appName=mern-ari-blog
