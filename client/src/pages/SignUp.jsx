7b7295e12b33781dc1e07423319fe9fba98672be
Falode Tobi <thobbytosin11@gmail.com>
2024-04-12 17:08:45 +0100

    add functionality to sign up

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,52 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Button, Label, TextInput, Alert, Spinner } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "../styles";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      // console.log(res);
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        return setErrorMessage(data.message);
+        // return setLoading(false);
+      }
+      if (res.ok) {
+        setTimeout(() => {
+          navigate("/sign-in");
+        }, 2500);
+      }
+      setLoading(false);
+    } catch (error) {
+      setErrorMessage(error);
+    }
+  };
   return (
     <div className={`${styles.paddingX} min-h-screen mt-20 font-poppins`}>
       <div className=" flex md:flex-row flex-col md:items-center justify-center max-w-3xl mx-auto gap-4">
@@ -21,10 +64,15 @@ const SignUp = () => {
           </p>
         </div>
         <div className=" flex-1">
-          <form className="flex flex-col gap-4">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             <div>
               <Label value="Username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Email" />
@@ -32,14 +80,31 @@ const SignUp = () => {
                 type="email"
                 placeholder="name@company.com"
                 id="email"
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value="Password" />
-              <TextInput type="password" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone={"greenToBlue"} outline>
-              Sign Up
+            <Button
+              gradientDuoTone={"greenToBlue"}
+              type="submit"
+              disabled={loading}
+              outline
+            >
+              {loading ? (
+                <span>
+                  <Spinner size="sm" /> Loading...
+                </span>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
           <div className=" mt-4">
@@ -48,6 +113,11 @@ const SignUp = () => {
               Sign In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
