bcdd461dba553a928a570b1b2c2b77c4b1725c1f
Falode Tobi <thobbytosin11@gmail.com>
2024-04-15 12:06:45 +0100

    add dashboard sidebar

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,37 @@
+import { Sidebar } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { HiUser, HiArrowRight } from "react-icons/hi";
+
+const DashSidebar = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    setTab(tabFromUrl);
+  }, [location.search]);
+
+  return (
+    <Sidebar className=" w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to={"/dashboard?tab=profile"}>
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item icon={HiArrowRight}>Sign Out</Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+};
+
+export default DashSidebar;
