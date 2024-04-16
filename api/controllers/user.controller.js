47a0c8a38751aec47d490b5b6a14187d992be03c
Falode Tobi <thobbytosin11@gmail.com>
2024-04-16 19:44:16 +0100

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -26,10 +26,13 @@ export const update = async (req, res, next) => {
         errorHandler(400, "Email field must be at least 6 characters")
       );
   }
+
   if (req.body.username?.includes(" "))
     return next(errorHandler(400, "Username cannot contain spaces"));
+
   if (req.body.username !== req.body.username?.toLowerCase())
     return next(errorHandler(400, "Username must be in lowercase"));
+
   if (!req.body.username?.match(/^[a-zA-z0-9]+$/))
     return next(
       errorHandler(400, "Username can only contain letters and numbers")
@@ -51,6 +54,17 @@ export const update = async (req, res, next) => {
 
     const { password: pass, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId)
+    return next(errorHandler(400, "You are not allowed to delete this user"));
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted");
   } catch (error) {
     next(error);
   }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { test, update } from "../controllers/user.controller.js";
+import { deleteUser, test, update } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.put("/update/:userId", verifyUser, update);
+router.delete("/delete/:userId", verifyUser, deleteUser);
 
 export default router;
