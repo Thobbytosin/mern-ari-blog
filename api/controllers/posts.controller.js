6f9f67c6eb3cb23adf670ef9c750f7b89dc29520
Falode Tobi <thobbytosin11@gmail.com>
2024-04-18 14:03:38 +0100

    add create post api route

---
+++ api/controllers/posts.controller.js
@@ -0,0 +1,26 @@
+import Post from "../models/post.model.js";
+import { errorHandler } from "../utils/error.js";
+
+export const createPost = async (req, res, next) => {
+  console.log(req.user);
+  if (!req.user.isAdmin)
+    return next(errorHandler(403, "You are not permitted to create a post"));
+  if (!req.body.title || !req.body.content)
+    return next(errorHandler(400, "Please provide all required fields"));
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .replace(/[^a-zA-Z0-9-]/g, "");
+
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
