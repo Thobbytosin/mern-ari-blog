418fc49d3a4896f1ff90a0370e134e750b3ed0c1
Falode Tobi <thobbytosin11@gmail.com>
2024-04-18 16:15:06 +0100

    add create post image upload functionality

--- api/controllers/posts.controller.js
+++ api/controllers/posts.controller.js
@@ -2,7 +2,6 @@ import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const createPost = async (req, res, next) => {
-  console.log(req.user);
   if (!req.user.isAdmin)
     return next(errorHandler(403, "You are not permitted to create a post"));
   if (!req.body.title || !req.body.content)
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,10 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles";
-import { Button, FileInput, Select, TextInput } from "flowbite-react";
+import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const CreatePost = () => {
+  const [file, setFile] = useState(null);
+  const [fileUploadProgress, setFileUploadProgress] = useState(null);
+  const [fileUploadError, setFileUploadError] = useState(null);
+  const [fileUploadSuccess, setFileUploadSuccess] = useState(null);
+  const [formData, setFormData] = useState({});
+  console.log(file);
+  // console.log(`progress:${fileUploadProgress}%`);
+  // console.log(`Error: ${fileUploadError}`);
+  // console.log(`success: ${fileUploadSuccess}`);
+  // console.log(formData);
+
+  const handleImageUpload = async () => {
+    // setFileUploadSuccess(null);
+    // setFileUploadError(null);
+
+    try {
+      if (!file) return setFileUploadError("Please select an image");
+
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // upload progress
+          const uploadProgress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setFileUploadProgress(Math.round(uploadProgress));
+        },
+        // upload error
+        (error) => {
+          setFileUploadError(
+            "Error uploading image. Image must be less than 2MB"
+          );
+          setFileUploadSuccess(null);
+        },
+        // upload success
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setFileUploadSuccess("Image uploaded successfully");
+            setFileUploadError(null);
+            setFileUploadProgress(null);
+            setFormData({ ...formData, image: downloadURL });
+          });
+        }
+      );
+    } catch (error) {
+      setFileUploadError("Image Upload failed");
+    }
+  };
   return (
     <div
       className={`${styles.padding} min-h-screen max-w-3xl mx-auto text-center font-poppins mb-12`}
@@ -27,16 +88,42 @@ const CreatePost = () => {
           </Select>
         </div>
         <div className=" border-teal-500 border-dotted border-4 p-3 flex gap-4 items-center justify-between ">
-          <FileInput type="file" accept="image/*" />
+          <FileInput
+            type="file"
+            accept="image/*"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
           <Button
             type="button"
             gradientDuoTone={"greenToBlue"}
             size="sm"
             outline
+            onClick={handleImageUpload}
+            disabled={fileUploadProgress && fileUploadProgress < 100}
           >
-            Upload Image
+            {fileUploadProgress && fileUploadProgress < 100 ? (
+              <div className=" w-12 h-12">
+                <CircularProgressbar
+                  value={fileUploadProgress}
+                  text={`${fileUploadProgress || 0}%`}
+                />
+              </div>
+            ) : (
+              "Upload Image"
+            )}
           </Button>
         </div>
+        {fileUploadSuccess && (
+          <Alert color="success">{fileUploadSuccess}</Alert>
+        )}
+        {fileUploadError && <Alert color="failure">{fileUploadError}</Alert>}
+        {formData.image && (
+          <img
+            src={formData.image}
+            alt="upload"
+            className="w-full h-72 object-cover"
+          />
+        )}
         <ReactQuill
           theme="snow"
           placeholder="Write something.."
